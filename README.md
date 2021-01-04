2aca0bab57604c157284c7a7e6e00a024fc3800d
essence <essence.from@gmail.com>
2021-01-04 20:10:10 +0800

    图片打分系统

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Picture Scoring System
 ### Dependencies
-- nodejs 14.15.3 </br> mirror[http://npm.taobao.org/mirrors/node/v14.15.3/]
+- nodejs 14.15.3 </br> [mirror](http://npm.taobao.org/mirrors/node/v14.15.3/)
 - jQuery
---
+++ index.js
@@ -0,0 +1,38 @@
+// reference:https://blog.csdn.net/Lai_im/article/details/106964782
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+
+    let pathname = url.parse(req.url).pathname;
+    pathname = pathname.endsWith('.png') ? 'imgs/' + pathname : pathname;
+    let realPath = path.join('./', pathname);
+    console.log(pathname, "client requets for: "+realPath);
+
+    if(pathname[1]=='u'){
+        console.log("huida:"+req.url)
+        res.end("<html><body>Thank you very much. BiXin~<br><img src='./bixin.jpg'></body></html>");
+        
+    }
+    else{
+        fs.readFile(realPath, (err, doc) => {
+            if (err != null) {
+                res.writeHead(404, {
+                    'content-type': 'text/html; charset=utf8'
+                });
+                res.end('404 Not Found');
+                return;
+            }
+            
+            res.end(doc);
+        });
+    }
+});
+
+app.listen(3000);
+console.log('启动服务器成功');
